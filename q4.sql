b47abbceb17e80f5fe81a71b378a9500d2eced66
Wu Dongnuo <wudongnu@teach.cs.toronto.edu>
2025-04-05 21:46:24 -0400

    finish q4

--- q4.sql
+++ q4.sql
@@ -1,2 +1,8 @@
 create or replace view ptcp_times as
-select email_id, 
+select mid, count(gsid) as times
+from Participant
+group by mid;
+
+select * from Member natural join (
+select mid, times from ptcp_times
+where times = (select max(times) from ptcp_times)) a;
