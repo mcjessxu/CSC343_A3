5891cfd0b9f0c78dfc3c5990df38f38108a6b82e
Wu Dongnuo <wudongnu@teach.cs.toronto.edu>
2025-04-05 20:36:42 -0400

    modify schema

--- data/settle.py
+++ data/settle.py
@@ -1,21 +1,21 @@
 import csv
 member = []
-with open('Member.csv', newline='') as m:
+with open('BoardGame.csv', newline='') as m:
     spamreader = csv.reader(m)
     for row in spamreader:
-        member.append([row[0], row[2]])
+        member.append([row[0], row[1]])
 
 fix = [] 
-with open('CommitteeFellow.csv', newline='') as em:
+with open('GameSession.csv', newline='') as em:
     spamreader = csv.reader(em)
     for row in spamreader:
         fix.append(row)
-
+print(fix)
 for i in fix:
     for j in member:
-        if i[3] == j[1]:
+        if i[1] == j[1]:
             print(j)
-            i[3] = j[0]
+            i[1] = j[0]
 
 with open('GameSession.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
